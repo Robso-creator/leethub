56e38b511d096def6c3d8281e9055d5f7810c89c
Robson Allef Alencar Sampaio <68085792+Robso-creator@users.noreply.github.com>
2024-10-01 19:16:45 -0300

    Time: 58 ms (96.31%), Space: 18.2 MB (57.61%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,28 @@
+"""
+ input   a,  , b, (, c, ), d, '', '',
+    stck 8
+  i  8
+
+output  ab(c)d
+"""
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s) # O(n)
+        stck = [] # O(n)
+
+        for i, let in enumerate(s):
+            if let == '(':
+                stck.append(i)
+            elif let == ')':
+                if stck:
+                    stck.pop()
+                else:
+                    s[i] = ''
+        while stck:
+            s[stck.pop()] = '' #stck.pop return the index saved on the stack, then replace it
+        
+        return ''.join(s)
+
+#: n = amount of char in the string
+#: T: O(n)
+#: S: O(n)
