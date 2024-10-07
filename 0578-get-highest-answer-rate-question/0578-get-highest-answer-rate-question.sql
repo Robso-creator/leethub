e94037ed79059f22b05b044802bf189bccbed3fd
Robson Allef Alencar Sampaio <68085792+Robso-creator@users.noreply.github.com>
2024-10-07 16:03:51 -0300

    Time: 236 ms (57.67%), Space: 0B (100%) - LeetHub

--- 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
+++ 0578-get-highest-answer-rate-question/0578-get-highest-answer-rate-question.sql
@@ -1,11 +1,14 @@
 -- Write your PostgreSQL query statement below
-select 
-    question_id as survey_log
-    from (
+
+with tbl_rank as (
 select
     question_id, 
     count(answer_id)::numeric / count(question_id) rate
 from surveylog
 group by question_id 
-order by rate desc, question_id asc limit 1
+order by rate desc, question_id asc 
 )
+select 
+    question_id as survey_log
+from tbl_rank 
+limit 1
