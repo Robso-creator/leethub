771246cabe5e9673597fa85b97b269c576c3cf32
Robson Allef Alencar Sampaio <68085792+Robso-creator@users.noreply.github.com>
2025-02-03 15:05:39 -0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        saw_dict_t = {}
+        saw_dict_s = {}
+
+        for wrd_s in s:
+            if wrd_s not in saw_dict_s:
+                saw_dict_s[wrd_s] = 1
+            else:
+                saw_dict_s[wrd_s] += 1
+
+
+        for wrd_t in t:
+            if wrd_t not in saw_dict_t:
+                saw_dict_t[wrd_t] = 1
+            else:
+                saw_dict_t[wrd_t] += 1
+
+        for k, v in saw_dict_s.items():
+            if k not in saw_dict_t or saw_dict_t[k] != v:
+                return False
+
+        return True
+
+        #S: O(N)
+        #T: O(N)
